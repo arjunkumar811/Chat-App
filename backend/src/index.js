66a2e6db36decb14b370d04ff3c75a8ae40bb283
arjunkumar811 <kumararjun26401@gmail.com>
2025-03-27 17:13:43 +0530

    Added auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,9 @@ import express from 'express'
 
 
 const app = express();
+import authRouter from './routes/auth.route.js'
 
+app.use("/api/auth", authRouter);
 
 app.listen(3000, () => {
     console.log('server started')
