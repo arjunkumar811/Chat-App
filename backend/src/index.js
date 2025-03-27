1e89265952c62297f3f16097ca3618001371d216
arjunkumar811 <kumararjun26401@gmail.com>
2025-03-27 17:41:05 +0530

    Added db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 import express from 'express'
-
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express();
 import authRouter from './routes/auth.route.js'
+import { connectDB } from './lib/db.js';
+
 
 app.use("/api/auth", authRouter);
 
-app.listen(3000, () => {
-    console.log('server started')
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log('server started at ', PORT)
+    connectDB()
 })
