8e5295d12a74a33176f3f42b9db89b9924e2a323
arjunkumar811 <kumararjun26401@gmail.com>
2025-04-12 20:11:10 +0530

    Added Routers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,28 @@
+import { Route, Routes } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
+import SettingsPage from "./pages/SettingsPage"
+import ProfilePage from "./pages/ProfilePage"
 
 function App() {
   
   return (
     <>
-      <div className="bg-blue-500">hi there</div>
+     
+     <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<SettingsPage />} />
+        <Route path="/" element={<ProfilePage />} />
+
+         </Routes>
+     </div>
     </>
   )
 }
---
+++ frontend/src/pages/SignUpPage.tsx
@@ -0,0 +1,11 @@
+
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <h1>Sign Up Page</h1>
+    </div>
+  )
+}
+
+export default SignUpPage
