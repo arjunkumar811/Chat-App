22242b506d995d3874821acd02d31fea58dc19b0
arjunkumar811 <kumararjun26401@gmail.com>
2025-04-19 13:57:52 +0530

    Added cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,12 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 dotenv.config()
+import cors from 'cors'
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 const app = express();
 app.use(express.json())
 app.use(cookieParser())
